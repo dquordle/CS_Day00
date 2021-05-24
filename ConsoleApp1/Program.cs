ee1877d7a8d218d89386c32fddf44ab9c51a9ead
dquordle <fedka.bogatov@gmail.com>
2021-05-24 18:53:06 +0300

    Initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+
+int wrong_arg()
+{
+    Console.WriteLine("Wrong argument");
+    return (1);
+}
+
+double get_percent(double sum, double rate, int year, int month)
+{
+    double percent = sum * rate * DateTime.DaysInMonth(year, month);
+    double koef;
+    if (DateTime.IsLeapYear(year))
+        koef = 100 * 366;
+    else
+        koef = 100 * 365;
+    percent /= koef;
+    return percent;
+}
+
+double get_monthly(double sum, double rate, int term)
+{
+    double i = rate / (12 * 100);
+    double i_plus_one_n = Math.Pow(1 + i, term);
+    double monthly = sum * i * i_plus_one_n / (i_plus_one_n - 1);
+    return monthly;
+}
+
+double ft_sum_red(double sum, double rate, int term, int sel_M, double payment)
+{
+    double init_sum = sum;
+    double monthly = get_monthly(sum, rate, term);
+    int year = DateTime.Today.Year;
+    int month = DateTime.Today.Month;
+    double res = 0;
+    for (int it = 0; it < term; it++)
+    {
+        double percent = get_percent(sum, rate, year, month);
+        sum += percent;
+        sum -= monthly;
+        res += monthly;
+        if (it == sel_M - 1)
+        {
+            sum -= payment;
+            monthly = get_monthly(sum, rate, term - sel_M);
+            res += payment;
+        }
+        month++;
+        if (month == 13)
+        {
+            month = 1;
+            year++;
+        }
+    }
+    res += sum;
+    return res - init_sum;
+}
+
+double get_months(double sum, double monthly, double rate)
+{
+    double i = rate / (12 * 100);
+    double months = Math.Log(monthly / (monthly - i * sum), 1 + i);
+    return months;
+}
+
+double ft_time_red(double sum, double rate, int term, int sel_M, double payment)
+{
+    double init_sum = sum;
+    double monthly = get_monthly(sum, rate, term);
+    int year = DateTime.Today.Year;
+    int month = DateTime.Today.Month;
+    double res = 0;
+    for (int it = 0; it < term; it++)
+    {
+        double percent = get_percent(sum, rate, year, month);
+        sum += percent;
+        sum -= monthly;
+        res += monthly;
+        if (it == sel_M - 1)
+        {
+            sum -= payment;
+            it = -1;
+            term = (int)Math.Ceiling(get_months(sum, monthly, rate));
+            Console.WriteLine(term);
+            res += payment;
+            sel_M = -1;
+        }
+        month++;
+        if (month == 13)
+        {
+            month = 1;
+            year++;
+        }
+        Console.WriteLine($"monthly: {monthly}, percent: {percent}, ost: {sum}");
+    }
+    res += sum;
+    return res - init_sum;
+}
+
+if (args.Length != 5)
+{
+    Console.WriteLine("Wrong number of arguments");
+    return 1;
+}
+
+if (!double.TryParse(args[0], out double sum) || sum < 0)
+    return (wrong_arg());
+if (!double.TryParse(args[1], out double rate) || rate < 0)
+    return (wrong_arg());
+if (!int.TryParse(args[2], out int term) || term < 0)
+    return (wrong_arg());
+if (!int.TryParse(args[3], out int sel_M) || sel_M < 0)
+    return (wrong_arg());
+if (!double.TryParse(args[4], out double payment) || payment < 0)
+    return (wrong_arg());
+// Console.WriteLine($"Credit sum is {sum}");
+
+double sum_red_op = Math.Round(ft_sum_red(sum, rate, term, sel_M, payment), 2);
+double time_red_op = Math.Round(ft_time_red(sum, rate, term, sel_M, payment), 2);
+string res;
+if (sum_red_op < time_red_op)
+    res = "Уменьшение срока выгоднее уменьшения платежа";
+else if (sum_red_op > time_red_op)
+    res = "Уменьшение платежа выгоднее уменьшения срока";
+else
+   res = "Уменьшение платежа равно уменьшению срока";
+Console.WriteLine(res);
+Console.WriteLine($"Переплата при уменьшении платежа : {sum_red_op}р.");
+Console.WriteLine($"Переплата при уменьшении срока : {time_red_op}р.");
+if (sum_red_op != time_red_op)
+    Console.WriteLine($"{res} на {Math.Round(Math.Abs(sum_red_op - time_red_op), 2)}р.");
+return 0;
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.IO;
+
+int LevenshteinDistance(string s1, string s2)
+{
+    s1 = s1.ToLower();
+    s2 = s2.ToLower();
+    int[,] Matrix = new int[s1.Length + 1, s2.Length + 1];
+
+    for (int i = 0; i <= s1.Length; i++)
+    {
+        for (int j = 0; j <= s2.Length; j++)
+            Matrix[i, j] = 0;
+    }
+
+    for (int i = 0; i <= s1.Length; i++)
+        Matrix[i, 0] = i;
+    for (int j = 0; j <= s2.Length; j++)
+        Matrix[0, j] = j;
+
+    int substitution_Cost;
+    for (int i = 1; i <= s1.Length; i++)
+    {
+        for (int j = 1; j <= s2.Length; j++)
+        {
+            substitution_Cost = s1[i - 1] == s2[j - 1] ? 0 : 1;
+
+            Matrix[i, j] = Math.Min(Matrix[i - 1, j] + 1, Matrix[i, j - 1] + 1);
+            Matrix[i, j] = Math.Min(Matrix[i - 1, j - 1] + substitution_Cost, Matrix[i, j]);
+        }
+    }
+
+    return Matrix[s1.Length, s2.Length];
+}
+
+Console.WriteLine(LevenshteinDistance("boSunday", "Sunday"));
+if (!File.Exists("/Users/dquordle/RiderProjects/Solution2/ConsoleApp2/us.txt"))
+{
+    Console.WriteLine("Файл со списком имен не найден");
+    return 1;
+}
+
+string[] names = File.ReadAllLines("/Users/dquordle/RiderProjects/Solution2/ConsoleApp2/us.txt");
+
+Console.WriteLine("Enter Name: ");
+string user = Console.ReadLine();
+if (string.IsNullOrEmpty(user))
+    Console.WriteLine("Your name was not found");
+string[] dist_1 = new string[names.Length];
+string[] dist_2 = new string[names.Length];
+string[] dist_3 = new string[names.Length];
+int d1, d2, d3;
+d1 = d2 = d3 = 0;
+foreach (var name in names)
+{
+    if (name == user)
+    {
+        Console.WriteLine($"Hello, {name}!");
+        return 0;
+    }
+
+    int distance = LevenshteinDistance(name, user);
+    
+    if (distance == 1)
+    {
+        dist_1[d1] = name;
+        d1++;
+    }
+    else if (distance == 2)
+    {
+        dist_2[d2] = name;
+        d2++;
+    }
+    if (distance == 3)
+    {
+        dist_3[d3] = name;
+        d3++;
+    }
+}
+
+int i = 0;
+while (d1 > 0 || d2 > 0 || d3 > 0)
+{
+    string option;
+    if (d1 > 0)
+    {
+        option = dist_1[i++];
+        if (i == d1)
+            i = d1 = 0;
+    }
+    else if (d2 > 0)
+    {
+        option = dist_2[i++];
+        if (i == d2)
+            i = d2 = 0;
+    }
+    else
+    {
+        option = dist_3[i++];
+        if (i == d3)
+            i = d3 = 0;
+    }
+    Console.WriteLine($"Did you mean \"{option}\"? Y/N");
+    string answer = Console.ReadLine();
+    while (answer != "Y" && answer != "N")
+    {
+        Console.WriteLine("Wrong input, try again!");
+        answer = Console.ReadLine();
+    }
+
+    if (answer == "Y")
+    {
+        Console.WriteLine($"Hello, {option}!");
+        return 0;
+    }
+}
+Console.WriteLine("Your name was not found.");
+return 0;
